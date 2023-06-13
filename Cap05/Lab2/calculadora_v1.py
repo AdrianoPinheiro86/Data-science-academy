18d9967c59087b9d8586529f211fc305719d1b4e
adriano_martins <adriano_martins@hst.com.br>
2023-06-13 15:08:25 -0300

    Calculadora

--- Cap05/Lab2/calculadora_v1.py
+++ Cap05/Lab2/calculadora_v1.py
@@ -6,3 +6,58 @@
 print("\n******************* Calculadora em Python *******************")
 
 
+# Função para realizar a adição
+def adicao(num1, num2):
+    return num1 + num2
+
+# Função para realizar a subtração
+def subtracao(num1, num2):
+    return num1 - num2
+
+# Função para realizar a multiplicação
+def multiplicacao(num1, num2):
+    return num1 * num2
+
+# Função para realizar a divisão
+def divisao(num1, num2):
+    return num1 / num2
+
+# Loop principal
+while True:
+    print("\nSelecione a operação:")
+    print("1 - Adição")
+    print("2 - Subtração")
+    print("3 - Multiplicação")
+    print("4 - Divisão")
+    print("5 - Sair")
+
+    escolha = input("Digite a opção desejada (1/2/3/4/5): ")
+
+    if escolha == '5':
+        print("Encerrando a calculadora...")
+        break
+
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+
+    if escolha == '1':
+        resultado = adicao(num1, num2)
+        operacao = "+"
+    elif escolha == '2':
+        resultado = subtracao(num1, num2)
+        operacao = "-"
+    elif escolha == '3':
+        resultado = multiplicacao(num1, num2)
+        operacao = "*"
+    elif escolha == '4':
+        if num2 != 0:
+            resultado = divisao(num1, num2)
+            operacao = "/"
+        else:
+            print("Erro: divisão por zero!")
+            continue
+    else:
+        print("Opção inválida. Tente novamente.")
+        continue
+
+    print("O resultado da operação", num1, operacao, num2, "é:", resultado)
--- Cap05/atualizado cap05/Lab2/calculadora_v1.py
+++ Cap05/atualizado cap05/Lab2/calculadora_v1.py
@@ -5,4 +5,61 @@
 
 print("\n******************* Calculadora em Python *******************")
 
+# Função para realizar a adição
+def adicao(num1, num2):
+    return num1 + num2
+
+# Função para realizar a subtração
+def subtracao(num1, num2):
+    return num1 - num2
+
+# Função para realizar a multiplicação
+def multiplicacao(num1, num2):
+    return num1 * num2
+
+# Função para realizar a divisão
+def divisao(num1, num2):
+    return num1 / num2
+
+# Loop principal
+while True:
+    print("\nSelecione a operação:")
+    print("1 - Adição")
+    print("2 - Subtração")
+    print("3 - Multiplicação")
+    print("4 - Divisão")
+    print("5 - Sair")
+
+    escolha = input("\nDigite a opção desejada (1/2/3/4/5): ")
+
+    if escolha == '5':
+        print("\nEncerrando a calculadora...\n")
+        break
+
+    num1 = float(input("\nDigite o primeiro número: "))
+    num2 = float(input("\nDigite o segundo número: "))
+
+    if escolha == '1':
+        resultado = adicao(num1, num2)
+        operacao = "+"
+    elif escolha == '2':
+        resultado = subtracao(num1, num2)
+        operacao = "-"
+    elif escolha == '3':
+        resultado = multiplicacao(num1, num2)
+        operacao = "*"
+    elif escolha == '4':
+        if num2 != 0:
+            resultado = divisao(num1, num2)
+            operacao = "/"
+        else:
+            print("Erro: divisão por zero!")
+            continue
+    else:
+        print("Opção inválida. Tente novamente.")
+        continue
+
+    print("\n=> O resultado da operação é:", num1, operacao, num2, "=", resultado)
+
+
 
